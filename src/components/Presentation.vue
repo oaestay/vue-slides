200dd06cf01809c8fc14aebea8adfaca8b0c1da7
oaestay <oaestay@uc.cl>
2019-06-07 09:00:51 -0400

    wip

---
+++ src/components/Presentation.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="presentation eg-slideshow">
+    <slide>
+      <h1>Hola</h1>
+      <p>Mundo</p>
+      <eg-code-block class="eg-code-block" lang="html">
+        &lt;h1&gt;Hola&lt;/h1&gt;
+        &lt;p&gt;Mundo&lt;/p&gt;
+      </eg-code-block>
+      <eg-code-block class="eg-code-block" lang="javascript">
+        export default {
+          mixins: [Slideshow],
+          props: {
+            msg: String,
+          },
+        };
+      </eg-code-block>
+    </slide>
+    <slide>
+      <h1>Chao</h1>
+      <p>Mundo</p>
+    </slide>
+  </div>
+</template>
+
+<script>
+import { Slideshow } from 'eagle.js';
+
+export default {
+  mixins: [Slideshow],
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue';
+import Eagle, { CodeBlock, Options, Presenter } from 'eagle.js';
+import hljs from 'highlight.js/lib/highlight';
+
+import xml from 'highlight.js/lib/languages/xml';
+import javascript from 'highlight.js/lib/languages/javascript';
 import App from './App.vue';
+
+import 'highlight.js/styles/atom-one-dark.css';
+
+import 'eagle.js/dist/eagle.css';
+
 import './registerServiceWorker';
 
+Eagle.use(CodeBlock);
+Eagle.use(Presenter);
+
 Vue.config.productionTip = false;
+
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('javascript', javascript);
+Options.hljs = hljs;
+
+Vue.use(Eagle);
 
 new Vue({
   render: h => h(App),
